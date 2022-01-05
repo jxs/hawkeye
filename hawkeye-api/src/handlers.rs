1b63615d2f8c3d06651708f41d070539c403e3b5
Lyle Scott III <lyle.scott@cbsinteractive.com>
2022-01-05 12:11:36 -0800

    Update hawkeye-api/src/handlers.rs
    Co-authored-by: Rafael Carício <rafael@caricio.com>

--- hawkeye-api/src/handlers.rs
+++ hawkeye-api/src/handlers.rs
@@ -473,7 +473,7 @@ pub async fn stop_watcher(id: String, client: Client) -> Result<impl warp::Reply
         Status::Running => {
             // Stop watcher / replicas to 0
             let mut patch_params = PatchParams::default();
-            patch_params.field_manager = Option::from("hawkeye_api".to_string());
+            patch_params.field_manager = Some("hawkeye_api".to_string());
 
             let deployment_scale_json = json!({
                 "apiVersion": "autoscaling/v1",
