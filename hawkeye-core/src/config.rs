e55d47ad6813c02f33acd2ca81710699a469c304
Lyle Scott III <lyle.scott@cbsinteractive.com>
2022-01-07 15:12:25 -0800

    Update hawkeye-core/src/config.rs

--- hawkeye-core/src/config.rs
+++ hawkeye-core/src/config.rs
@@ -8,7 +8,7 @@ const HAWKEYE_ENV_ENV: &str = "HAWKEYE_ENV";
 lazy_static! {
     pub static ref HAWKEYE_ENV: String = std::env::var(HAWKEYE_ENV_ENV).unwrap_or("local".into());
     pub static ref SLATE_URL_FILE_EXTENSIONS: Vec<String> = std::env::var(SLATE_URL_FILE_EXTS_ENV)
-        .unwrap_or("jpg,jpeg,png".into())
+        .unwrap_or_else(|_| "jpg,jpeg,png".into())
         .split(",")
         .map(|a| a.trim().to_string())
         .collect();
